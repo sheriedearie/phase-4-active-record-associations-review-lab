a4a53c78f51be48fdaf8470d5e475d3b6347443c
sheridan <sheridanmaustad@gmail.com>
2022-04-28 11:48:00 -0600

    commit tree 4a98b77597995a8e38e61ce8bc8226cc16e0bdaa

--- db/migrate/20210506153111_create_taxis.rb
+++
@@ -1,8 +0,0 @@
-class CreateTaxis < ActiveRecord::Migration[6.1]
-  def change
-    create_table :taxis do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20210506153127_create_rides.rb
+++
@@ -1,8 +0,0 @@
-class CreateRides < ActiveRecord::Migration[6.1]
-  def change
-    create_table :rides do |t|
-
-      t.timestamps
-    end
-  end
-end
